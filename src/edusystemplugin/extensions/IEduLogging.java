ab0638ca94601ffe54c654aa7082e45bf4492aeb
Shawn Mathes <shawnmathes@gmail.com>
2014-03-03 19:28:43 -0600

    Expanded the log entries.

--- src/edusystemplugin/extensions/IEduLogging.java
+++ src/edusystemplugin/extensions/IEduLogging.java
@@ -3,5 +3,8 @@ package edusystemplugin.extensions;
 public interface IEduLogging {
 
 	public abstract void writeLog(String message);
-	
+	public abstract void writeDebug(String message);
+	public abstract void writeInfo(String message);
+	public abstract void writeError(String message);
+	public abstract void writeAudit(String message);
 }
--- src/edusystemplugin/extensions/LoggingService.java
+++ src/edusystemplugin/extensions/LoggingService.java
@@ -16,13 +16,36 @@ public class LoggingService {
 		}
 	}
 
+	public static void writeDebug(String message) {
+		for (IEduLogging logger : loggers) {
+			logger.writeDebug(message);
+		}
+	}
+
+	public static void writeInfo(String message) {
+		for (IEduLogging logger : loggers) {
+			logger.writeInfo(message);
+		}
+	}
+
+	public static void writeError(String message) {
+		for (IEduLogging logger : loggers) {
+			logger.writeError(message);
+		}
+	}
+
+	public static void writeAudit(String message) {
+		for (IEduLogging logger : loggers) {
+			logger.writeAudit(message);
+		}
+	}
+
 	private static ArrayList<IEduLogging> create() {
 		ArrayList<IEduLogging> loggers = new ArrayList<IEduLogging>();
 		IConfigurationElement[] config = Platform.getExtensionRegistry()
 				.getConfigurationElementsFor("edusystemplugin.logging");
 		try {
 			for (IConfigurationElement e : config) {
-				System.out.println("Evaluating extension");
 				final Object o = e.createExecutableExtension("class");
 				if (o instanceof IEduLogging) {
 					loggers.add((IEduLogging) o);
@@ -31,7 +54,6 @@ public class LoggingService {
 			
 			return loggers;
 		} catch (CoreException ex) {
-			System.out.println(ex.getMessage());
 			return null;
 		}
 	}
